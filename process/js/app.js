652eef6c18cc9c27daf1dc9709a7d440bf959eda
10K-LinesOfCode <10k.linesofcoode@gmail.com>
2020-08-06 23:34:40 +0530

    Updated

--- process/js/app.js
+++ process/js/app.js
@@ -1,18 +1,30 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 //Main Interface
-var MainInterface = React.CreateClass({
+//Step 1
+var MainInterface = React.createClass({
     //Information to render
     render: function () {
-        return <h1>Pet Appointments</h1>
+        return (
+
+            <div>
+            <h1>Pet Appointments</h1>
+            <ul>
+                <li>Prabhu's Dog 3.30 PM</li>
+                <li>Prabhu's Cat 3.30 PM</li>
+                <li>Prabhu's Cow 3.30 PM</li>
+            </ul>
+            </div>
+
+        )
     }
 
 });
 
-
+//Step2 
 ReactDom.render(
 
-    <MainInterface/>,
+    <MainInterface />,
     document.getElementById("petAppointments")
 
 
