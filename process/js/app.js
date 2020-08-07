67b24acf16df214d3e5007d5ad1d9abbd7b5974b
10K-LinesOfCode <10k.linesofcoode@gmail.com>
2020-08-07 11:31:38 +0530

    updated

--- process/js/app.js
+++ process/js/app.js
@@ -2,21 +2,45 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var MainInterface = React.createClass({
-  render: function() {
-    return (
-      <div className="interface">
-        <h1>Pet Appointments</h1>
-        <ul>
-          <li>Buffy 3:30 PM</li>
-          <li>Spot 8:30 PM</li>
-          <li>Goldie 3:50 PM</li>
-        </ul>
-      </div>
-    )
-  } //render
+
+    getInitialState: function () {
+
+        return {
+            title:'Appointments',
+            show :true
+        }
+
+    },
+
+
+    render: function () {
+        var showTitle;
+        if (this.state.show) {
+            showTitle="new "
+        }
+
+        var displayList ={
+            display:this.state.show ? 'block':'none',
+            color:'red'
+        }
+
+
+
+        return (
+            <div className="interface">
+                <h1>{showTitle}
+                    {this.state.title}</h1>
+                <ul style={displayList}>
+                    <li>Buffy 3:30 PM</li>
+                    <li>Spot 8:30 PM</li>
+                    <li>Goldie 3:50 PM</li>
+                </ul>
+            </div>
+        )
+    } //render
 }); //MainInterface
 
 ReactDOM.render(
-  <MainInterface />,
-  document.getElementById('petAppointments')
+    <MainInterface />,
+    document.getElementById('petAppointments')
 ); //render
